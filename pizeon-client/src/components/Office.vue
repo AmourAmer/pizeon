4d2a9e97969b39f8ce16fa766b8199c6d604eba4
Amour <pizeon@tuta.io>
2024-04-25 22:35:16 +0800

    ugly office

--- pizeon-client/src/components/Office.vue
+++ pizeon-client/src/components/Office.vue
@@ -1,3 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { toRef } from "vue";
+const submitForm = function () {
+  // TODO don't forget timestamp and signature
+  formData.value = { stamp: "", board: "", body: "", heading: "" };
+};
+const formData = toRef({ stamp: "", board: "", body: "", heading: "" });
+</script>
 
-<template></template>
+<template>
+  <form @submit.prevent="submitForm">
+    <!-- TODO prevent keyStroke -->
+    <label for="heading">Heading:</label>
+    <input type="heading" id="heading" v-model="formData.heading" />
+    <textarea id="body" v-model="formData.body"></textarea>
+    <label for="stamp">Stamp:</label>
+    <!-- TODO should choose from a list -->
+    <input type="text" id="stamp" v-model="formData.stamp" />
+    <label for="board">Show at:</label>
+    <input type="board" id="board" v-model="formData.board" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
