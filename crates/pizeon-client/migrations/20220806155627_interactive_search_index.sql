1186c9fd62a07fc903abaf33ee9882356c46522e
Amour <pizeon@tuta.io>
2024-04-30 12:56:29 +0800

    sql

--- crates/pizeon-client/migrations/20220806155627_interactive_search_index.sql
+++
@@ -1,6 +0,0 @@
--- Interactive search filters by command then by the max(timestamp) for that
--- command. Create an index that covers those
-create index if not exists idx_history_command_timestamp on history(
-	command,
-	timestamp
-);
---
+++ crates/pizeon-client/migrations/20240430090735_create-notices.sql
@@ -0,0 +1,19 @@
+create table if not exists notices (
+  blocked boolean not null,
+	id text primary key,
+	timestamp integer not null,
+	body text not null,
+	versions text not null,
+	deleted_at integer,
+  expires_at integer
+
+	-- unique(timestamp, cwd, command)
+);
+
+create index if not exists idx_notice_timestamp on notices(timestamp);
+create index if not exists idx_notice_blocked on notices(blocked);
+
+create index if not exists idx_history_blocked_timestamp on notices(
+	blocked,
+	timestamp
+);
