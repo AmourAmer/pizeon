42149cce9628a64fe0e0007adda345533a989d26
Amour <pizeon@tuta.io>
2024-05-01 16:36:19 +0800

    which_repo

--- pizeon-bubble/src-tauri/src/api/repos.rs
+++ pizeon-bubble/src-tauri/src/api/repos.rs
@@ -1,14 +1,27 @@
 use super::{db, notices::Repo};
-use pizeon_client::database::Database;
+use pizeon_client::{database::Database, notice::Notice};
 
 #[tauri::command]
 pub async fn get_bill(repo: Repo) -> Vec<String> {
+    // I don't know why I wrote this inside fn.
+    fn which_repo(notice: &Notice) -> Repo {
+        if notice.deleted_at.is_some() {
+            return Repo::Junk;
+        }
+        if notice.blocked {
+            return Repo::Blocked;
+        }
+        Repo::Fresh
+        // TODO: Fridge
+    }
+
     let db = db().await.unwrap();
     db.list(None, false)
         .await
         .unwrap()
         .iter()
+        .filter(|notice| which_repo(notice) == repo)
         .map(|notice| notice.id.0.clone())
         .collect()
-    // FIXME: of course
+    // FIXME: Did I write actually Repo filter already? Or some enhancement?
 }
