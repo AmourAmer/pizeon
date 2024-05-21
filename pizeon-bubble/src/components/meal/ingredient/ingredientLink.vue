2cbc739e5e2c3a37503292380426bbb839788e80
Amour <pizeon@tuta.io>
2024-05-21 12:48:35 +0800

    async QR code

--- pizeon-bubble/src/components/meal/ingredient/ingredientLink.vue
+++ pizeon-bubble/src/components/meal/ingredient/ingredientLink.vue
@@ -1,41 +1,35 @@
 <script setup lang="ts">
 // TODO: fix type annotation
-import { onMounted } from "vue";
 import { stringMap } from "@utils/type";
 import QRCode from "qrcode";
+import { QRCodeToDataURLOptions } from "qrcode";
+import { computedAsync } from "@vueuse/core";
 const props = defineProps<{
   datum: stringMap;
 }>();
 
-onMounted(() => {
-  var opts = {
-    errorCorrectionLevel: "H",
-    type: "image/jpeg",
-    quality: 0.3,
-    margin: 1,
-    color: {
-      dark: "#010599FF",
-      light: "#FFBF60FF",
-    },
-  };
+const opts = {
+  errorCorrectionLevel: "L",
+  type: "image/svg",
+  margin: 1,
+  color: {
+    dark: "#000",
+    light: "#fff6",
+  },
+};
 
-  QRCode.toDataURL(
-    props.datum?.body || "no link found",
-    opts,
-    function (err, url) {
-      if (err) throw err;
-
-      var img = document.getElementById("image");
-      console.log(img);
-      img.src = url;
-    },
-  );
-});
+const src = computedAsync(
+  () =>
+    QRCode.toDataURL(props.datum?.body || "/", opts as QRCodeToDataURLOptions),
+  "",
+);
 </script>
 <template>
   <div>
-    <a :href="datum?.body">{{ datum?.body }}</a>
-    <!-- FIXME: unique id -->
-    <img id="image" />
+    <!-- TODO: toggle -->
+    <a :href="datum?.body">
+      {{ datum?.body || "no url found" }}
+    </a>
+    <img :src="src" />
   </div>
 </template>
